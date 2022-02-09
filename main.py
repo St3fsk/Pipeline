56bbec03f150702dd2243a387eace3de5af1d86a
Stef <99083931+St3fsk@users.noreply.github.com>
2022-02-09 20:40:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# THIS WILL FAIL!
+#def hello():
+    #print("hi")
+
+
+#def bye()
+    #print("bye")
+
+
+#print(hello())
+
+# THIS WILL GO THROUGH!
 def hello():
     print("hi")
 
 
-def bye()
+def bye():
     print("bye")
 
 
